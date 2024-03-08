00599c243150f133c125c485058ada909c2939ff
Maria F Mero <m.fernanda.m.acosta@gmail.com>
2024-03-08 11:06:38 -0300

    Workshop of Intro a JS!

---
+++ intro_js/app.js
@@ -0,0 +1,167 @@
+// tipos de datos
+
+/*//String: Cadena de caracteres
+console.log ("Maria")
+console.log('Fernanda')
+console.log('Mero')
+
+console.log("$10")
+console.log("10")
+
+
+//Number
+console.log(10)
+console.log(-10)
+console.log(-10.5)*/
+
+/* Boolean */
+console.log(true)
+console.log(false)
+
+/* undefined */
+/*console.log(undefined) */
+
+/*null */
+/*console.log(null)*/
+
+//variables
+/*let nombre = "Maria"
+
+nombre = nombre + "Fernanda"
+
+const apellido = Mero
+
+const edad = 24
+
+edad =29
+
+const mascota = "true"
+
+let nombreCompleto = nombre + "" + apellido
+console.log( nombreCompleto)
+
+
+let presentacion = ` Soy ${nombre} ${apellido} y tengo ${edad} años `
+
+console.log( presentacion)
+
+console.log('El mentor dijo: "participen" ')
+
+console.log( "I´m Mafer")
+
+console.log("El mentor n\ dijo: \"participen\" ")*/
+
+//Operadores //
+
+console.log( 20 + 30 )
+console.log( -30 -20 )
+console.log( 5 * 10 )
+console.log( 10/2 )
+
+console.log( 2**3 )
+
+console.log( 10 % 2 )
+
+// Comparacion //
+
+/*console.log( "30 < 20:" , 30 < 20 )
+console.log( "30 > 20:" , 30 > 20 )
+console.log ( "30 <= 30:" , 30 <= 30 )
+console.log ( "30 >= 30:" , 30 >= 30 )
+console.log (' "3" == 3:' , "3" == 3 )
+console.log (' "3" === 3:' , "3" === 3 )
+console.log (' "3" != 3:' , "3" != 3)
+console.log (' "3" !== 3:' , "3" !== 3)*/
+
+
+/* Operadores logicos */
+
+/* && (AND) || (OR)  ! (NOT) */
+
+/*console.log( "Tabla de verdad AND")
+console.log( "true && true:", true && true  )
+console.log( "true && false:", true && false )
+console.log( "false && true:" , false && true )
+console.log( "false && false:" , false && false )
+
+console.log( "Tabla de verdad OR")
+console.log( "true || true:", true ||  true  )
+console.log( "true ||  false:", true ||  false )
+console.log( "false ||  true:" , false ||  true )
+console.log( "false ||  false:" , false ||  false )
+
+console.log( "Tabla de verdad NOT")
+console.log( "!true:", !true  )
+console.log( "false:", !false )
+
+console.log ( (!false) && true )
+
+console.log ( false && true && true )
+
+console.log ( false || true || true ) */
+
+
+/* OBJETO */
+
+const mentor = {
+    nombre: "Mafer",
+    apellido: "Mero",
+    edad: 22,
+    genero: "F",
+    mentor: true,
+    tecnologia: "JavaScript",
+    mascota: {
+        nombre: "Nila",
+        animal: "Gato",
+    },
+
+
+    saludar(){
+        console.log ('Hola a todos')
+    }
+}
+
+mentor.nombre = "Nicolas Catriel"
+
+
+console.log(mentor)
+console.log( mentor.apellido)
+
+console.log( mentor.apellido === apellido )
+
+console.log( mentor.mascota.animal)
+
+/* Tipos de datos*/
+/* Primitivos 
+    string
+    number
+    boolean
+    undefined
+    null
+
+    bigInt
+    symbol
+*/
+
+//  Array  //
+
+const listaAlumnos = [ "Agustin", "Agustina", "Alejandra"]
+
+const listaNumeros = [ 10, 20, 30, 40, 50, 60 ]
+
+const listaNumerosPares = [ 0, 2, 4, 6, 8, 10]
+
+console.log ( listaAlumnos[1])
+console.log ( listaAlumnos[3])
+console.log ( listaAlumnos.length)
+
+
+
+
+
+
+
+
+
+
+
